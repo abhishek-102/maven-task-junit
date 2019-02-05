450e1288756a00324489a2a7328c3e66df704540
Abhishek <abhishekchauhan@qainfotech.com>
2019-02-05 16:26:11 +0530

    commit after first test

---
+++ maven-project/src/main/java/gamewithletters/App.java
@@ -0,0 +1,34 @@
+package gamewithletters;
+
+import java.util.ArrayList;
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String consecutive (String s)
+    {
+
+        System.out.println("hahahaahah");
+        if(s.equalsIgnoreCase("Abhishek"))
+        {
+            return "Matched";
+        }
+
+    return "Not-found";
+    }
+
+
+
+        
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        App obj = new App();
+        //String actuall =obj.consecutive("harshit");
+        //System.out.println(actuall);
+    }
+}
